809119ca2d9cc892c5a9a1f66f8590f7c84526fe
Erica <erica.salcedo@thomsonreuters.com>
2016-01-26 15:40:41 +0800

    update all files

--- app/src/main/java/com/test/trshuttlefinder/ActivityTwo.java
+++ app/src/main/java/com/test/trshuttlefinder/ActivityTwo.java
@@ -4,23 +4,18 @@ package com.test.trshuttlefinder;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
-
 import android.support.v7.app.AppCompatActivity;
-
 import android.util.Log;
-import android.view.ContextThemeWrapper;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
-//import android.widget.TextView;
-
-
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class ActivityTwo extends AppCompatActivity {
 
-    static String _From,_To,_Time;
+    static String _From, _To, _Time;
     TextView text_Result;
     SQLite dbHandler;
     public Typeface KnowledgeFont;
@@ -34,48 +29,40 @@ public class ActivityTwo extends AppCompatActivity {
         _To = getIntent().getStringExtra("To1");
         _Time = getIntent().getStringExtra("Time1");
 
-        dbHandler = new SQLite(this,null,null,1);
+        dbHandler = new SQLite(this, null, null, 1);
 
         KnowledgeFont = Typeface.createFromAsset(getAssets(), "fonts/Knowledge-Medium.ttf");
         ListView list = (ListView) findViewById(R.id.SCHEDULE);
-        //TextView tResult = (TextView) findViewById(R.id.VAN_No);
-        //tResult.setTypeface(KnowledgeFont);
 
         ArrayList<HashMap<String, String>> mylist = new ArrayList<HashMap<String, String>>();
         HashMap<String, String> map = new HashMap<String, String>();
 
         String dbString = dbHandler.databaseToString();
         String dbRowValues[] = dbString.split(System.getProperty("line.separator"));
-        String tmpString[]=null;
+        String tmpString[] = null;
 
 
-        for (int i=0; i < dbRowValues.length; i++){
-        //for (int i=0; i < 50; i++){
+        for (int i = 0; i < dbRowValues.length; i++) {
+
             map = new HashMap<String, String>();
-            if ( i==0 ){
+            if (i == 0) {
                 TextView tResult = (TextView) findViewById(R.id.data0);
 
                 tResult.setTextColor(Color.parseColor("#F7F7F7"));
                 tResult.setTypeface(KnowledgeFont);
 
                 tResult.setText(dbRowValues[0] + "\n" + dbRowValues[1] +
-                        "\n" + "Number of Records: " + dbRowValues.length);
+                        "\n" + "Number of Records: " + (dbRowValues.length - 2));
                 i = 1;
                 map.put("van", "VAN #");
                 map.put("from", "FROM");
                 map.put("to", "TO");
                 map.put("etd", "ETD");
-                map.put("bldg","BLDG");
-            }
-            else {
-                tmpString=dbRowValues[i].split(",");
+                map.put("bldg", "BLDG");
+            } else {
+                tmpString = dbRowValues[i].split(",");
 
-                /*Log.d("brey_rec0", tmpString[0]);
-                Log.d("brey_rec1", tmpString[1]);
-                Log.d("brey_rec2", tmpString[2]);
-                Log.d("brey_rec3", tmpString[3]);
-                Log.d("brey_rec4", tmpString[4]);
-                Log.d("brey_rec5", tmpString[5]);*/
+
                 map.put("van", tmpString[0] + "  " + tmpString[1]);
                 map.put("from", tmpString[2]);
                 map.put("to", tmpString[3]);
@@ -88,14 +75,10 @@ public class ActivityTwo extends AppCompatActivity {
 
 
         SimpleAdapter mSchedule = new SimpleAdapter(this, mylist, R.layout.raw,
-                new String[] {"van", "from", "to","etd","bldg"}, new int[] {R.id.VAN_No, R.id.FROM_CELL, R.id.TO_CELL, R.id.ETD, R.id.BLDG});
+                new String[]{"van", "from", "to", "etd", "bldg"}, new int[]{R.id.VAN_No, R.id.FROM_CELL, R.id.TO_CELL, R.id.ETD, R.id.BLDG});
         list.setAdapter(mSchedule);
         Log.d("breyerror_1", "list.setAdapter(mSchedule);");
 
-        TextView tResult1 = (TextView) findViewById(R.id.VAN_No);
-        tResult1.setTypeface(KnowledgeFont);
-
     }
-
 
 }
