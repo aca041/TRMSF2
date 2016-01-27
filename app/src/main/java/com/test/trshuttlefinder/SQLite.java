5a1b9dc892864f813fccc541d251814bfc405909
aca041 <aca041@gmail.com>
2016-01-27 17:32:09 +0800

    - clean up codes that makes app crash for android 4.1

--- app/src/main/java/com/test/trshuttlefinder/SQLite.java
+++ app/src/main/java/com/test/trshuttlefinder/SQLite.java
@@ -162,38 +162,6 @@ public class SQLite extends SQLiteOpenHelper  {
         return tst;
     }
 
-    public class CSVFile {
-        InputStream inputStream;
-
-        public CSVFile(InputStream inputStream){
-            this.inputStream = inputStream;
-        }
-
-        public List read(){
-            List resultList = new ArrayList();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-            try {
-                String csvLine;
-                while ((csvLine = reader.readLine()) != null) {
-                    String[] row = csvLine.split(",");
-                    resultList.add(row);
-                }
-            }
-            catch (IOException ex) {
-                throw new RuntimeException("Error in reading CSV file: "+ex);
-            }
-            finally {
-                try {
-                    inputStream.close();
-                }
-                catch (IOException e) {
-                    throw new RuntimeException("Error while closing input stream: "+e);
-                }
-            }
-            return resultList;
-        }
-    }
-
     public static String[] Load(File file)
     {
         FileInputStream fis = null;
